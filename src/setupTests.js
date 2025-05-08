4a95c8637eb76119039cf490c0457e487b7f18ab
Koki Riho <riho.dare@gmail.com>
2025-05-08 12:44:22 -0700

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,19 @@
+/** 
+ * プロジェクト: https://portfolio-wise.com/ 
+ * ファイルパス: src/setupTests.js 
+ * 
+ * 作成者: Koki Riho （https://github.com/Rih0z） 
+ * 作成日: 2025-03-01 10:00:00 
+ * 
+ * 更新履歴: 
+ * - 2025-03-01 10:00:00 Koki Riho 初回作成
+ * - 2025-05-08 11:20:00 Koki Riho ファイルヘッダーを追加
+ * 
+ * 説明: 
+ * テスト環境のセットアップを行うファイル。
+ * Jest DOM拡張を含み、DOMノードに対するカスタムマッチャーを追加する。
+ * Create React Appのデフォルト設定に含まれており、テストスイートの実行前に自動的に実行される。
+ */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
