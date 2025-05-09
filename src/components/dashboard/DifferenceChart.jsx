e72d8c90314c3329458b51e6c7c37cb730dbfb91
Koki Riho <riho.dare@gmail.com>
2025-05-08 17:38:17 -0700

    Update DifferenceChart.jsx

--- src/components/dashboard/DifferenceChart.jsx
+++ src/components/dashboard/DifferenceChart.jsx
@@ -1,3 +1,19 @@
+/** 
+ * プロジェクト: https://portfolio-wise.com/ 
+ * ファイルパス: src/components/dashboard/DifferenceChart.jsx 
+ * 
+ * 作成者: Koki Riho （https://github.com/Rih0z） 
+ * 作成日: 2025-05-08 14:30:00 
+ * 
+ * 更新履歴: 
+ * - 2025-05-08 14:30:00 Koki Riho 初回作成
+ * 
+ * 説明: 
+ * ポートフォリオの現在配分と目標配分の差分を表示する棒グラフコンポーネント。
+ * 各銘柄のパーセントポイント差を視覚化し、過不足を一目で把握できるようにする。
+ * recharts ライブラリを使用して実装。
+ */
+
 import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import { usePortfolioContext } from '../../hooks/usePortfolioContext';
