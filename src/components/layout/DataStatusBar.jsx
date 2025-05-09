ee3e38909888531ec65bf3ba987183427af073f6
Koki Riho <riho.dare@gmail.com>
2025-05-08 17:40:36 -0700

    Update DataStatusBar.jsx

--- src/components/layout/DataStatusBar.jsx
+++ src/components/layout/DataStatusBar.jsx
@@ -1,3 +1,18 @@
+/** 
+ * プロジェクト: https://portfolio-wise.com/ 
+ * ファイルパス: src/components/layout/DataStatusBar.jsx 
+ * 
+ * 作成者: Koki Riho （https://github.com/Rih0z） 
+ * 作成日: 2025-05-08 14:30:00 
+ * 
+ * 更新履歴: 
+ * - 2025-05-08 14:30:00 Koki Riho 初回作成
+ * 
+ * 説明: 
+ * データの状態を表示するステータスバーコンポーネント。
+ * 最終更新日時、為替レート情報、データ更新が必要かどうかの警告を表示する。
+ * データ更新ボタンも含み、更新中の状態も視覚的に表示する。
+ */
 import React from 'react';
 import { usePortfolioContext } from '../../hooks/usePortfolioContext';
 import { formatDate } from '../../utils/formatters';
