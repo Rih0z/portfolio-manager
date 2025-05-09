c506f0f06768ab48aff343bcc2ad2469161cf8ad
Koki Riho <riho.dare@gmail.com>
2025-05-08 18:53:52 -0700

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,3 +1,18 @@
+
+/** 
+ * プロジェクト: https://portfolio-wise.com/ 
+ * ファイルパス: src/hooks/useAuth.js
+ * 
+ * 作成者: Koki Riho （https://github.com/Rih0z） 
+ * 作成日: 2025-03-15 13:45:22 
+ * 
+ * 更新履歴: 
+ * - 2025-03-15 13:45:22 Koki Riho 初回作成
+ * 
+ * 説明: 
+ * AuthContextから認証状態と認証関連機能を取得するためのカスタムフック。
+ * このフックを使用することで、コンポーネントは認証情報にアクセスできる。
+ */
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 
