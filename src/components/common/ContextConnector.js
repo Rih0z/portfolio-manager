30fea6e2174108075532e22d068e772a281315d8
Koki Riho <riho.dare@gmail.com>
2025-05-08 17:36:25 -0700

    Update ContextConnector.js

--- src/components/common/ContextConnector.js
+++ src/components/common/ContextConnector.js
@@ -1,3 +1,18 @@
+/** 
+ * プロジェクト: https://portfolio-wise.com/ 
+ * ファイルパス: src/components/common/ContextConnector.js 
+ * 
+ * 作成者: Koki Riho （https://github.com/Rih0z） 
+ * 作成日: 2025-05-08 14:30:00 
+ * 
+ * 更新履歴: 
+ * - 2025-05-08 14:30:00 Koki Riho 初回作成
+ * 
+ * 説明: 
+ * AuthContextとPortfolioContextを連携させるコネクタコンポーネント。
+ * 循環参照を防ぐためにuseRefを使用して一方向参照を実装する。
+ */
+
 import React, { useEffect } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import { usePortfolioContext } from '../hooks/usePortfolioContext';
