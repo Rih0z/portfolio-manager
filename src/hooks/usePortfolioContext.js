5dd8a12cb2710d1c7596d0f9217f4584dfd5b594
Koki Riho <riho.dare@gmail.com>
2025-05-08 18:54:12 -0700

    Update usePortfolioContext.js

--- src/hooks/usePortfolioContext.js
+++ src/hooks/usePortfolioContext.js
@@ -1,3 +1,18 @@
+/** 
+ * プロジェクト: https://portfolio-wise.com/ 
+ * ファイルパス: src/hooks/usePortfolioContext.js
+ * 
+ * 作成者: Koki Riho （https://github.com/Rih0z） 
+ * 作成日: 2025-03-15 14:30:22 
+ * 
+ * 更新履歴: 
+ * - 2025-03-15 14:30:22 Koki Riho 初回作成
+ * 
+ * 説明: 
+ * PortfolioContextからポートフォリオデータと関連機能を取得するためのカスタムフック。
+ * このフックを使用することで、コンポーネントはポートフォリオ情報とその操作メソッドにアクセスできる。
+ */
+
 import { useContext } from 'react';
 import { PortfolioContext } from '../context/PortfolioContext';
 
