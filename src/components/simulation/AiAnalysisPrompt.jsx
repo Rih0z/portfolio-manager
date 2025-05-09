39f241606d3184e6ba427438d6a66c778a48f1a4
Koki Riho <riho.dare@gmail.com>
2025-05-08 17:44:05 -0700

    Update AiAnalysisPrompt.jsx

--- src/components/simulation/AiAnalysisPrompt.jsx
+++ src/components/simulation/AiAnalysisPrompt.jsx
@@ -1,3 +1,18 @@
+/** 
+ * プロジェクト: https://portfolio-wise.com/ 
+ * ファイルパス: src/components/simulation/AiAnalysisPrompt.jsx 
+ * 
+ * 作成者: Koki Riho （https://github.com/Rih0z） 
+ * 作成日: 2025-05-08 14:30:00 
+ * 
+ * 更新履歴: 
+ * - 2025-05-08 14:30:00 Koki Riho 初回作成
+ * 
+ * 説明: 
+ * ポートフォリオデータを含むAI分析用プロンプトを生成するコンポーネント。
+ * 現在のポートフォリオ構成、目標配分、予算情報などを含むプロンプトを生成し、
+ * クリップボードにコピーしてAIアシスタントで使用できるようにする。
+ */
 import React, { useState } from 'react';
 import { usePortfolioContext } from '../../hooks/usePortfolioContext';
 import { formatCurrency, formatPercent, formatDate } from '../../utils/formatters';
