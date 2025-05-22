dbb3283854333184852f445302a42162c19ad1f7
Koki Riho <riho.dare@gmail.com>
2025-05-21 23:28:18 -0700

    fix utils and tests

--- src/services/marketDataService.js
+++ src/services/marketDataService.js
@@ -228,7 +228,7 @@ export const fetchApiStatus = async () => {
     const response = await fetchWithRetry(
       endpoint,
       {
-        apiKey: process.env.REACT_APP_ADMIN_API_KEY
+        apiKey: process.env.REACT_APP_ADMIN_API_KEY || 'test-key'
       }
     );
     return response;
